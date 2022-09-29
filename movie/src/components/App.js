238b3b4f76c62cb71c25ae30c7e817ba3b0334f2
sean-code <john.nganga@student.moringaschool.com>
2022-09-30 00:11:19 +0300

    Conflict

--- movie/src/components/App.js
+++ movie/src/components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-// import {Routes, Route} from "react-router-dom"
 import Series from "./Series";
 import './App.css'
-// import NavBar from "./NavBar";
 
 function App() {
   return (
