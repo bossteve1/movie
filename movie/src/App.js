9a4b9203a8acdbb11fe8cf1378271bc702c5e53d
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-09-29 10:52:29 +0300

    initial commit

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Comments from "./components/comments"
 function App() {
   return (
     <div className="App">
-      
+      <Comments/>
     </div>
   );
 }
---
+++ movie/src/components/comments.js
@@ -0,0 +1,22 @@
+
+function Comments(){
+
+    return (
+        <div>
+            <h2>Leave us your comment about our movies</h2>
+            <div>
+            <label>
+                Name
+                <input></input>
+            </label>
+            </div>
+            <div>
+            <label>
+                Comment
+                <textarea type="textarea" ></textarea>
+            </label>
+            </div>
+        </div>
+    )
+}
+export default Comments;
