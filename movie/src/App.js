75b94e8018eb476c497dc71379f758cb450761dc
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-09-29 23:44:40 +0300

    merge add movies to watchlist

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import AddMovie from './AddMovie';
 import './App.css';
+import Watchlist from './Components/Watchlist';
+import watch from "./data/data";
+
 
 function App() {
+  const [list, setList] = useState(watch)
+
   return (
     <div className="App">
       <AddMovie />
-      
+      <Watchlist list={list} setList={setList} />
     </div>
   );
 }
--- movie/src/index.js
+++ movie/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import App from "../src/Components/App";
 // import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
