8b0217b4cd79d1885315bd3f8807a0226e675f01
sean-code <john.nganga@student.moringaschool.com>
2022-09-29 19:32:57 +0300

    Edit information on the Db.json

--- movie/src/components/Series.js
+++ movie/src/components/Series.js
@@ -1,18 +1,22 @@
-import Items from '../data.json'
+import Items from '../Series.json'
 function Series(){
     return(
         <div>
-      {
-        Items.map(show =>{
-          return(
-            <div className="box"key={show.id}>
-              <h2>{show.title}</h2>
-              <h3>{show.director}</h3>
-              {show.seasons}
-            </div>
-          )
-        })
-
+            {
+            Items.map(show =>{
+            return(
+                <div className="box"key={show.id}>
+                <h2>{show.title}</h2>
+                <h3>{show.director}</h3>
+                {show.seasons}
+                <ul>
+                    {show.genres.map((category)=>(
+                        <li key={category}>{category}</li>
+                    ))}
+                </ul>
+                </div>
+            )
+            })
       }
         </div>
     )
