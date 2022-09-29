d3a3abcf02d13a3b478705c40bc547811d7b7674
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-09-29 11:14:29 +0300

    initial commit

--- movie/src/components/comments.js
+++ movie/src/components/comments.js
@@ -1,20 +1,30 @@
-
+import "./comments.css"
+import CommentsList from "./CommentsList";
 function Comments(){
 
     return (
-        <div>
+        <div className="comments-sec">
             <h2>Leave us your comment about our movies</h2>
             <div>
             <label>
                 Name
-                <input></input>
+                <input placeholder="Enter your Name"></input>
+            </label>
+            </div>
+            <div>
+            <label>
+                Favorite Movie
+                <input placeholder="Enter Favorite movie"></input>
             </label>
             </div>
             <div>
             <label>
                 Comment
-                <textarea type="textarea" ></textarea>
+                <textarea type="textarea" placeholder="Leave a comment here" ></textarea>
             </label>
+            </div>
+            <div>
+                <CommentsList />
             </div>
         </div>
     )
