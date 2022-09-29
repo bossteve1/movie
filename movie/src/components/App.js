c42a09d3568a3aef0bb2296b01ee86919f814261
sean-code <john.nganga@student.moringaschool.com>
2022-09-29 19:09:24 +0300

    Create a Series Component

--- movie/src/components/App.js
+++ movie/src/components/App.js
@@ -1,25 +1,12 @@
 import React from "react";
-import Items from '../data.json'
+import Series from "./Series";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Series</h1>
-
-      {
-        Items.map(show =>{
-          return(
-            <div className="box"key={show.id}>
-              {show.image}
-              <h2>{show.title}</h2>
-              <h3>{show.director}</h3>
-              {show.seasons}
-            </div>
-          )
-        })
-
-      }
-
+      <Series />
     </div>
   );
 }
