50a39b57a041e91b98e1f5884958eb5d06d651fa
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-09-29 22:09:09 +0300

    add styles

--- movie/src/components/comments.js
+++ movie/src/components/comments.js
@@ -1,31 +1,58 @@
 import "./comments.css"
-import CommentsList from "./CommentsList";
+import React,{useState} from "react";
 function Comments(){
-
+const [name,setName]=useState("")
+const [favoriteMovie,setFavoriteMovie]=useState("")
+const [comment,setComent]=useState("")
+const [post,setPost]=useState("")
+function handleSubmit(e){
+e.preventDefault()
+setPost([name,favoriteMovie,comment])
+}
     return (
         <div className="comments-sec">
             <h2>Leave us your comment about our movies</h2>
+            <form onSubmit={handleSubmit} >
             <div>
             <label>
                 Name
-                <input placeholder="Enter your Name"></input>
+                <input 
+                name="name"
+                placeholder="Enter your Name"
+                value={name} 
+                onChange={(e)=>{setName(e.target.value)}}></input>
             </label>
             </div>
             <div>
             <label>
                 Favorite Movie
-                <input placeholder="Enter Favorite movie"></input>
+                <input 
+                placeholder="Enter Favorite movie" 
+                value={favoriteMovie}
+                onChange={(e)=>{setFavoriteMovie(e.target.value)}}></input>
             </label>
             </div>
             <div>
             <label>
                 Comment
-                <textarea type="textarea" placeholder="Leave a comment here" ></textarea>
+                <textarea type="textarea" 
+                placeholder="Leave a comment here" 
+                value={comment} 
+                onChange={(e)=>{setComent(e.target.value)}}></textarea>
             </label>
             </div>
             <div>
-                <CommentsList />
+                <button type="submit">Submit</button>
             </div>
+            </form>
+            <div>
+        <h2>Our Customers comments</h2>
+        <div>
+            <h3>{post[0]}</h3>
+            <h4>{post[1]}</h4>
+            <p>{post[2]}</p>
+        </div>
+    </div>
         </div>
     )
 }
